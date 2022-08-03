a7d7610ebf4b7f1b5f50ad6dfbc8dff5f9e444dc
Masha Rodionova <rodionovamv@gmai.com>
2022-08-03 18:36:36 +0300

    Added tests with junit4

---
+++ src/main/java/ru/netology/service/CashbackServiceTest.java
@@ -0,0 +1,91 @@
+package ru.netology.service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackServiceTest {
+    private CashbackService service = new CashbackService();
+
+    @Test
+    public void test1(){
+
+        int expected = 113;
+        int actual = service.remain(887);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2(){
+
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3(){
+
+        int expected = 999;
+        int actual = service.remain(1001);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test4(){
+
+        int expected = 1;
+        int actual = service.remain(999);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test5(){
+
+        int expected = 999;
+        int actual = service.remain(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test6(){
+
+        int expected = 1000;
+        int actual = service.remain(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test7(){
+
+        int expected = 1001;
+        int actual = service.remain(-1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test8(){
+
+        int expected = 0;
+        int actual = service.remain(2000);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test9(){
+
+        int expected = 700;
+        int actual = service.remain(300);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test10(){
+
+        int expected = 300;
+        int actual = service.remain(1700);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+}
